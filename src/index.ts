ae0a0d024442337f70eac9c17d06270ded6544b7
Cellivar <1441553+Cellivar@users.noreply.github.com>
2024-01-29 02:22:43 -0800

    Missed exporting devices...

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export * from './Error.js'
 export * from './Channel.js'
+export * from './Device.js'
+export * from './Error.js'
 export * from './InputListener.js'
 
 export * from './Usb/index.js'
