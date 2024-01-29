8920a80bb6ef692828924ccad4c68878fbaf1732
Cellivar <1441553+Cellivar@users.noreply.github.com>
2024-01-29 01:05:47 -0800

    Update boilerplate files

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
 /// <reference types="vitest" />
-import { resolve } from 'path';
+import path from 'path';
+import packageJson from './package.json';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'web-device-mux',
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: packageJson.name,
     },
     minify: false,
   },
-  plugins: [dts()],
+  define: {
+    'import.meta.vitest': 'undefined',
+  },
+  plugins: [dts(), eslint({
+    failOnError: false
+  })],
   test: {
-
+    includeSource: ['src/**/*.{js,ts}'],
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
   }
 });
